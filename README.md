c7f87e694c23a82c09aba1c77ddca501a1a18aa2
Carolina Neponucena <cneponucena@gmail.com>
2023-10-18 16:04:32 -0300

    responsividade pg2

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# Facebook
-# fruityVice2
+ # <img src="./img/cherry.png"  width="100" height="100" >
+
+<p> Venha conhecer FruitVyce! </p>
+<p> As frutas nos ajudam de diversas formas. Elas são fontes de hidratação, além de possuírem diversos minerais e vitaminas que são muito importantes para o nosso organismo. Pesquise e veja algumas imagens da fruta escolhida! </p>
+
+### 🍒 TEMAS
+- [Como funciona o FruitVyce?](#como-funciona?)
+- [Funções do FruitVyce](#funcões)
+- [Descrição do projeto FruitVyce](#descrição-do-projeto)
+- [Ferramentas Utilizadas](#ferramentas-utilizadasAPI)
+
+## 🍒 Como funciona o FruitVyce?
+<p>O FruitVyce funciona como uma espécie de galeria de frutas pesquisadas! O site fornece uma senha padrão para os usuários, que ao inserirem, conseguem acessar imagens referente as frutas que são procuradas.</p>
+
+## 🍒 Funcões do FruitVyce
+
+<p>• Permitir que o usuário acesse através de uma senha padrão 💻 </p>
+<img src="./img/loginnnnn.png" width="700px">
+<p>• Fornecer imagens das frutas pesquisadas 🔎</p>
+
+
+## 🍒 Descrição do projeto FruitVyce
+<p> Projeto desenvolvido no Curso de Desenvolvimento de Sistemas, orientado pelo professor Fernando Leonid pela matéria de Programação Web Front-End na Instituição Senai Prof° Vicente Amato - Jandira, SP 📍. O projeto pode ser acessado em GitHub Pages: </p> 
+<a><img src="https://play-lh.googleusercontent.com/PCpXdqvUWfCW1mXhH1Y_98yBpgsWxuTSTofy3NGMo9yBTATDyzVkqU580bfSln50bFU" width="30px"> https://carolneponucenaa.github.io/fruityVice2/</a>
+
+## 🍒 Ferramentas Utilizadas
+<p><strong>CSS, HTML, JS</strong> </p>
+<img src="https://upload.wikimedia.org/wikipedia/commons/1/10/CSS3_and_HTML5_logos_and_wordmarks.svg" width="80px">
+<img src="https://i0.wp.com/www.duomimikry.de/wp-content/uploads/2016/03/js-logo.png?fit=500%2C500&ssl=1" width="50px">
+
+
--- module/fruityPage.js
+++ module/fruityPage.js
@@ -1,33 +1,39 @@
-const apiKey = '39990416-2cc8ec2e94d954928f9ff6145'
-const numberOfImages = 5
-const themeEnt = document.getElementById('busca')
-const container = document.getElementById('imagem-container')
+const apiKey = '39990416-2cc8ec2e94d954928f9ff6145';
+const numberOfImages = 5;
+const themeEnt = document.getElementById('busca');
+const container = document.getElementById('imagem-container');
+const botao = document.getElementById('btnPesquisa');
+
+function pesquisa() {
+    const theme = themeEnt.value;
+    const apiUrl = `https://pixabay.com/api/?key=${apiKey}&q=${theme}&per_page=${numberOfImages}`;
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            container.innerHTML = '';
+
+            const photos = data.hits;
+
+            photos.forEach(photo => {
+                const imgcontainer = document.createElement('div');
+                const imgElement = document.createElement('img');
+                imgcontainer.classList.add('imgContainer');
+                imgElement.src = photo.webformatURL;
+                imgElement.alt = photo.user;
+                imgcontainer.appendChild(imgElement);
+                container.appendChild(imgcontainer);
+            });
+        })
+        .catch(error => {
+            console.error('Ocorreu um erro:', error);
+        });
+}
+
+botao.addEventListener('click', pesquisa);
 
 themeEnt.addEventListener('keyup', function(event) {
     if (event.key === 'Enter') {
-        const theme = themeEnt.value
-
-        const apiUrl = `https://pixabay.com/api/?key=${apiKey}&q=${theme}&per_page=${numberOfImages}`
-
-        fetch(apiUrl)
-            .then(response => response.json())
-            .then(data => {
-                container.innerHTML = ''
-
-                const photos = data.hits
-
-                photos.forEach(photo => {
-                    const imgcontainer = document.createElement('div')
-                    const imgElement = document.createElement('img')
-                    imgcontainer.classList.add('imgContainer')
-                    imgElement.src = photo.webformatURL
-                    imgElement.alt = photo.user
-                    imgcontainer.appendChild(imgElement)
-                    container.appendChild(imgcontainer)
-                })
-            })
-            .catch(error => {
-                console.error('Ocorreu um erro:', error)
-            })
+        pesquisa();
     }
-})
+});
